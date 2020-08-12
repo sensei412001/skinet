5ec804215ed17238d5885bf7d9769ea0fe09c4d7
juandustrious <sensei412001@gmail.com>
2020-08-13 00:15:41 +0800

    End of section 10

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { ShopModule } from './shop/shop.module';
+import { HomeModule } from './home/home.module';
 import { CoreModule } from './core/core.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
@@ -18,7 +18,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserAnimationsModule,
     HttpClientModule,
     CoreModule,
-    ShopModule,
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -5,6 +5,7 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map } from 'rxjs/operators';
 import { ShopParams } from '../shared/models/shopParams';
+import { IProduct } from '../shared/models/product';
 
 @Injectable({
   providedIn: 'root'
@@ -44,6 +45,10 @@ export class ShopService {
         return resp.body;
       })
     );
+  }
+
+  getProduct(id: number) {
+    return this.http.get<IProduct>(this.baseUrl + 'products/' + id);
   }
 
   getBrands() {
