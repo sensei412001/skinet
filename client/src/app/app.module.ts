b2588cc626d12ba67024acfdf16c1fb61a72dd26
juandustrious <sensei412001@gmail.com>
2020-08-14 13:06:22 +0800

    End of section 11

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,8 +1,9 @@
+import { ErrorInterceptors } from './core/interceptors/error.interceptor';
 import { HomeModule } from './home/home.module';
 import { CoreModule } from './core/core.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,7 +21,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     CoreModule,
     HomeModule
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptors, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
